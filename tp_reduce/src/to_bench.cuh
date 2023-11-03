7c7d308bb337c0dcb8a932432616fe614f666f0a
Vasco Sampaio <vasco.sampaio@epita.fr>
2023-11-03 15:23:26 +0000

    Deleted TPs

--- tp_reduce/src/to_bench.cuh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "cuda_tools/host_shared_ptr.cuh"
-
-void baseline_reduce(cuda_tools::host_shared_ptr<int> buffer,
-    cuda_tools::host_shared_ptr<int> total);
-
-void your_reduce(cuda_tools::host_shared_ptr<int> buffer,
-    cuda_tools::host_shared_ptr<int> total);
--- tp_scan_fetch_content/src/to_bench.cuh
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "cuda_tools/host_shared_ptr.cuh"
-
-void baseline_scan(cuda_tools::host_shared_ptr<int> buffer);
-
-void your_scan(cuda_tools::host_shared_ptr<int> buffer);
